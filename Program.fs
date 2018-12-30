b7e9cc7726d45aa2f6b19ad14b10003e14131611
KevinLamb <kevinlambert46@gmail.com>
2018-12-30 14:08:51 -0500

    Initial project files

---
+++ Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+
+let getStatus url = 
+    try
+        let response = Http.Request(url, silentHttpErrors = true)
+        Some response.StatusCode
+    with
+    | :? System.Net.WebException as ex -> None
+
+[<EntryPoint>]
+let main argv =
+    printfn "Input url to check status of:"
+    let url = Console.ReadLine()
+
+    let status = getStatus url
+
+    match status.IsSome with
+    | true -> printfn "HTTP Status: %A" status.Value
+    | _ -> printfn "An error occurred while getting status..."
+
+    let line = Console.ReadLine()
+    0 // return an integer exit code

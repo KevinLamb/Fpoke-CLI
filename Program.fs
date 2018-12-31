e28e60cf5c4f039a60178bb2f155ff33f0803f2e
KevinLamb <kevinlambert46@gmail.com>
2018-12-30 23:11:18 -0500

    Added more status code options

--- Program.fs
+++ Program.fs
@@ -1,24 +1,35 @@
 ﻿open System
 open FSharp.Data
 
+let goodCodes = [200 .. 299]
+let okCodes = [300 .. 399]
+let clientErrorCodes = [400 .. 499]
+let serverErrorCodes = [500 .. 599]
+
 let getStatus url = 
     try
         let response = Http.Request(url, silentHttpErrors = true)
         Some response.StatusCode
-    with
-    | :? System.Net.WebException as ex -> None
+    with :? System.Net.WebException as ex -> None
 
 [<EntryPoint>]
 let main arg =
 
     match arg with 
     | [|first|] -> 
-        let status = getStatus first
-        match status.Value with
-        | 200 -> printfn "The site is UP! HTTP Status: %i" status.Value
-        | 404 -> printfn "Site is not found or page is down... HTTP Status: %i" status.Value
-        | 500 -> printfn "SERVER ERROR: Site is DOWN! HTTP Status: %i" status.Value
-        | _ -> printfn "An error occurred while getting status..."
+
+        let statusCode = getStatus first        
+
+        match statusCode with
+        | Some status -> 
+                    match status with
+                        | s when List.contains s goodCodes = true -> printfn "The site is UP! HTTP Status: %i" status
+                        | s when List.contains s okCodes = true -> printfn "The site is UP! HTTP Status: %i" status
+                        | s when List.contains s clientErrorCodes = true -> printfn "Site is not found or page is down... HTTP Status: %i" status
+                        | s when List.contains s serverErrorCodes = true -> printfn "SERVER ERROR: Site is DOWN! HTTP Status: %i" status
+                        | _ -> printfn "An error occurred while getting status..."
+        | None -> printfn "How to use: \"fpoke http://your-url.com\""
+        
     | _ -> printfn "How to use: \"fpoke http://your-url.com\""
     
     0 
